9d0cba0ab827f8111136364800f26b788a3ab9ab
suryaNoble <suryamani11121@gmail.com>
2025-03-27 21:53:05 +0530

    lottie version error resolved final checkpoint commit

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,30 @@
-"use client"
+"use client";
 
-import Lottie from "lottie-react";
+// import Lottie from "lottie-react";
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
-};
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
 
-export default AnimationLottie;
+// export default AnimationLottie;
+
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), {
+  ssr: false,
+});
+
+export default function AnimationLottie({ animationPath }) {
+  return <Lottie animationData={animationPath} loop={true} />;
+}
